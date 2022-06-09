c16f60fa7fe9349b92d94aad8d1147cfcfb84bfa
Godson <nwankwogodsonc@gmail.com>
2022-06-09 13:11:45 +0100

    Tophead done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Spotify from "./Component/spot";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Spotify/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/spot.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./spot.css";
+
+const Spotify = () => {
+  return (
+    <div>
+      <div container>
+        <div className="App-header">
+          <img
+            className="sph"
+            src="https://sportify-git-master.itope84.vercel.app/img/bg-football.2d1b7b03.jpg"
+            alt="soccer"
+          />
+
+          <img
+            className="logo"
+            src="https://sportify-git-master.itope84.vercel.app/img/logo.efdde25b.png"
+            alt="logo"
+          />
+        </div>
+        <div className="logo"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Spotify;
