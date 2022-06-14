4259bc8920c9c260fed5b27164489639ee9b3831
Godson <nwankwogodsonc@gmail.com>
2022-06-14 18:14:22 +0100

    api link change

--- src/Component/league.js
+++ src/Component/league.js
@@ -1,24 +1,27 @@
-import { Container } from "@mui/system";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./league.css";
 import { fetchAllCompetitions } from "../Services/http";
 
 const League = () => {
-  const [match, setMatch] = useState([]);
+  const [league, setLeague] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const getMatch = async () => {
-    const { competitions } = await (await fetchAllCompetitions()).json();
-    setMatch(competitions);
-    console.log(competitions);
-  };
+  useEffect(() => {
+    fetchAllCompetitions()
+      .then((res) => {
+        setLeague(res.competitions);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <div>
       <container>
         <div className="container-lg">
-          <div class="container">
-            <Container></Container>
-          </div>
+          <div class="container"></div>
         </div>
       </container>
     </div>
