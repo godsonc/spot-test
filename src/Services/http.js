223621944129b6cf707810def19265b0ecc3911d
Godson <nwankwogodsonc@gmail.com>
2022-06-14 18:00:57 +0100

    api changes

--- src/Services/http.js
+++ src/Services/http.js
@@ -1,41 +1,6 @@
-import axios from "../axios/links";
+import http from "./links";
 
 export const fetchAllCompetitions = async () => {
-  try {
-    const url = "/competitions";
-    const res = await axios.get(url);
-    return res.data;
-  } catch (error) {
-    throw error;
-  }
+  const res = await http.get("/competitions/");
+  return res.competitions;
 };
-
-// export const getBookShelf = async () => {
-//   try {
-//     const url = "/bookshelf";
-//     const res = await axios.get(url);
-//     return res;
-//   } catch (error) {
-//     throw error;
-//   }
-// };
-
-// export const getAgentType = async () => {
-//   try {
-//     const url = "/agent_type";
-//     const res = await axios.get(url);
-//     return res;
-//   } catch (error) {
-//     throw error;
-//   }
-// };
-
-// export const getPerson = async () => {
-//   try {
-//     const url = "/person";
-//     const res = await axios.get(url);
-//     return res;
-//   } catch (error) {
-//     throw error;
-//   }
-// };
